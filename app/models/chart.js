032395f709b8ad7e76aa22944de5338d44586eb1
joe-adams <jadams12@gmail.com>
2015-06-13 20:35:44 -0500

    Pretty good, but now the chart is broken.

--- app/models/chart.js
+++ app/models/chart.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 import Stock from 'ember-proj/models/stock'
 
 export default DS.Model.extend({
+	//chartrows:DS.hasMany('chartrow'),
 	chartrows:DS.hasMany('chartrow'),
 	symbol:DS.attr('string'),
 	name:DS.attr('string')
--- app/routes/chart.js
+++ app/routes/chart.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function(id) {
+        debugger;
         var stockModel = this.store.getById('stock', id.id);
         var result = stockModel.toJSON();
-        return this.store.findQuery('chartrow', id.id).then(function(chartrows) {
-        	debugger;
+        return this.store.find('chartrow', id).then(function(chartrows) {
             result.chartrows=chartrows;
+            debugger;
             var record= this.store.createRecord('chart', result);
             return record;
         }.bind(this));
